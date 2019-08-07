ce6e4b8400ef7bfd794a451e3327cf32d41515a2
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2019-08-07 21:15:50 +0200

    :sparkles: Add inner dots

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,18 @@ const speeds = [
   getSpeedForDegreesPerSecond(360)
 ]
 
+interface CurveItem {
+  idX: number
+  idY: number
+}
+
+const curvesData: CurveItem[] = []
+for (let idX = 0; idX < data.length; idX++) {
+  for (let idY = 0; idY < data.length; idY++) {
+    curvesData.push({ idX, idY })
+  }
+}
+
 const getNextTimestamp = counterFactory()
 
 enum Dimension {
@@ -43,10 +55,6 @@ function loop () {
 requestAnimationFrame(loop)
 
 function render (renderSettings: RenderSettings) {
-  // for (let id = 0; id < data.length; id++) {
-  //   data[id] = getAngleInDegrees(renderSettings.timestamp, id)
-  // }
-
   svg
     .attr('preserveAspectRatio', 'xMinYMin meet')
     .attr('viewBox', `0 0 960 500`)
@@ -79,6 +87,24 @@ function render (renderSettings: RenderSettings) {
       .attr('cx', positioningFunctions.dotCircleX)
       .attr('cy', positioningFunctions.dotCircleY)
   }
+
+  const {
+    newDotCircles,
+    updatedDotCircles
+  } = getCurves()
+
+  const verticalPositioningFunctions = getCirclePositionFactory(renderSettings, Dimension.vertical)
+  const horizontalPositioningFunctions = getCirclePositionFactory(renderSettings, Dimension.horizontal)
+
+  newDotCircles
+    .attr('r', renderSettings.dotRadius)
+    .attr('cx', (d) => horizontalPositioningFunctions.dotCircleX(d.idX))
+    .attr('cy', (d) => verticalPositioningFunctions.dotCircleY(d.idY))
+
+  updatedDotCircles
+    .attr('r', renderSettings.dotRadius)
+    .attr('cx', (d) => horizontalPositioningFunctions.dotCircleX(d.idX))
+    .attr('cy', (d) => verticalPositioningFunctions.dotCircleY(d.idY))
 }
 
 function getCircles (dimension: Dimension) {
@@ -115,9 +141,35 @@ function getCircles (dimension: Dimension) {
   }
 }
 
+function getCurves () {
+  const groups = svg
+    .selectAll('g.curve')
+    .data(curvesData)
+
+  const newGroups = groups
+    .enter()
+    .append('g')
+    .attr('class', 'curve')
+  
+  const updatedGroups = groups
+
+  const newDotCircles = newGroups
+    .append('circle')
+    .attr('class', 'dot-circle')
+
+  const updatedDotCircles: d3.Selection<SVGCircleElement, CurveItem, d3.BaseType, unknown> = updatedGroups
+    .selectAll('circle.dot-circle')
+
+  return {
+    newDotCircles,
+    updatedDotCircles
+  }
+}
+
 function getCirclePositionFactory (renderSettings: RenderSettings, dimension: Dimension) {
   const circleDiameterWithoutStroke = renderSettings.circleRadiusWithoutStroke * 2
   const circleDiameterWithMargin = circleDiameterWithoutStroke + renderSettings.circleMargin
+  const circleRadiusWithMargin = renderSettings.circleRadiusWithoutStroke + renderSettings.circleMargin
 
   const functionsForDimension: {[K in Dimension]: {
     outerCircleX (d: number): number
@@ -141,48 +193,48 @@ function getCirclePositionFactory (renderSettings: RenderSettings, dimension: Di
 
   return functionsForDimension[dimension]
 
-  function outerCircleXVertical (d: number) {
-    return circleDiameterWithMargin * (d + 1) + renderSettings.circleRadiusWithoutStroke
-  }
-
-  function outerCircleYVertical (d: number) {
-    return renderSettings.circleRadiusWithoutStroke
-  }
-
   function outerCircleXHorizontal (d: number) {
-    return renderSettings.circleRadiusWithoutStroke
+    return circleDiameterWithMargin * (d + 1) + circleRadiusWithMargin
   }
 
   function outerCircleYHorizontal (d: number) {
-    return circleDiameterWithMargin * (d + 1) + renderSettings.circleRadiusWithoutStroke
+    return circleRadiusWithMargin
   }
 
-  function dotCircleXVertical (d: number) {
-    return (
-      circleDiameterWithMargin * (d + 1) + 
-      renderSettings.circleRadiusWithoutStroke + 
-      getOffsetFromCenter(renderSettings.timestamp, d, renderSettings.circleRadiusWithoutStroke).xOffset
-    )
+  function outerCircleXVertical (d: number) {
+    return circleRadiusWithMargin
   }
 
-  function dotCircleYVertical (d: number) {
-    return (
-      renderSettings.circleRadiusWithoutStroke * 1 +
-      getOffsetFromCenter(renderSettings.timestamp, d, renderSettings.circleRadiusWithoutStroke).yOffset
-    )
+  function outerCircleYVertical (d: number) {
+    return circleDiameterWithMargin * (d + 1) + circleRadiusWithMargin
   }
 
   function dotCircleXHorizontal (d: number) {
     return (
-      renderSettings.circleRadiusWithoutStroke * 1 +
+      circleDiameterWithMargin * (d + 1) + 
+      circleRadiusWithMargin + 
       getOffsetFromCenter(renderSettings.timestamp, d, renderSettings.circleRadiusWithoutStroke).xOffset
     )
   }
 
   function dotCircleYHorizontal (d: number) {
     return (
+      outerCircleYHorizontal(d) +
+      getOffsetFromCenter(renderSettings.timestamp, d, renderSettings.circleRadiusWithoutStroke).yOffset
+    )
+  }
+
+  function dotCircleXVertical (d: number) {
+    return (
+      outerCircleXVertical(d) + 
+      getOffsetFromCenter(renderSettings.timestamp, d, renderSettings.circleRadiusWithoutStroke).xOffset
+    )
+  }
+
+  function dotCircleYVertical (d: number) {
+    return (
       circleDiameterWithMargin * (d + 1) + 
-      renderSettings.circleRadiusWithoutStroke + 
+      circleRadiusWithMargin + 
       getOffsetFromCenter(renderSettings.timestamp, d, renderSettings.circleRadiusWithoutStroke).yOffset
     )
   }
